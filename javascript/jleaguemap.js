26c333bf4f539dd0a524fe92f211e2f6412b7cb4
dmarti03x1 <31459768+dmarti03x1@users.noreply.github.com>
2017-11-14 10:35:27 -0800

    inserting map
    div map assigned to var map

--- javascript/jleaguemap.js
+++ javascript/jleaguemap.js
@@ -1,5 +1,9 @@
 function initMap() {
 
  var myLatLng = {lat: 41.867959, lng: -87.614014};
-				
-      }
+	
+	var map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 8,
+  center: myLatLng
+  });
+  }
