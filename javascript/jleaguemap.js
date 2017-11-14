faee0537de1edc51cc6050893c5a18d1e3c88d0b
dmarti03x1 <31459768+dmarti03x1@users.noreply.github.com>
2017-11-14 12:05:16 -0800

    zoom 12
    setting map zoom  to 12

--- javascript/jleaguemap.js
+++ javascript/jleaguemap.js
@@ -3,7 +3,7 @@ function initMap() {
  var myLatLng = {lat: 41.867959, lng: -87.614014};
 	
 	var map = new google.maps.Map(document.getElementById('map'), {
-  zoom: 8,
+  zoom: 12,
   center: myLatLng
   });
   }
