3ade8275e9f3a8562594db51f8429f8e772bb9fd
dmarti03x1 <31459768+dmarti03x1@users.noreply.github.com>
2017-11-14 18:57:30 -0800

    adding 3 more markers
    will add one more marker

--- javascript/jleaguemap.js
+++ javascript/jleaguemap.js
@@ -3,6 +3,9 @@ function initMap() {
  var myLatLng = {lat: 41.867959, lng: -87.614014};
  var myLatLng2 = {lat: 41.835337, lng: -87.628208};
  var myLatLng3 = {lat: 41.867619, lng: -87.631044};
+ var myLatLng4 = {lat: 41.835063, lng: -87.663884};
+ var myLatLng5 = {lat: 41.859065, lng: -87.660573};
+ var myLatLng6 = {lat: 41.872777, lng: -87.675304};
 	
 	var map = new google.maps.Map(document.getElementById('map'), {
   zoom: 12,
@@ -25,5 +28,17 @@ function initMap() {
     position: myLatLng3,
     map: map,
     icon: 'amazonia.PNG'
+  });
+	
+	var marker = new google.maps.Marker({
+    position: myLatLng4,
+    map: map,
+    icon: 'adobe.PNG'
+  });
+	
+	var marker = new google.maps.Marker({
+    position: myLatLng5,
+    map: map,
+    icon: 'robet.PNG'
   });
   }
