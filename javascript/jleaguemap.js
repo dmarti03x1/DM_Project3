7b6323b163d5961ca9c1f8473e87f79748744bde
dmarti03x1 <31459768+dmarti03x1@users.noreply.github.com>
2017-11-14 18:35:44 -0800

    adding third marker
    this is the 3rd marker i added to map

--- javascript/jleaguemap.js
+++ javascript/jleaguemap.js
@@ -2,6 +2,7 @@ function initMap() {
 
  var myLatLng = {lat: 41.867959, lng: -87.614014};
  var myLatLng2 = {lat: 41.835337, lng: -87.628208};
+ var myLatLng3 = {lat: 41.867619, lng: -87.631044};
 	
 	var map = new google.maps.Map(document.getElementById('map'), {
   zoom: 12,
@@ -18,5 +19,11 @@ function initMap() {
     position: myLatLng2,
     map: map,
     icon: 'thebats.PNG'
+  });
+	
+	var marker = new google.maps.Marker({
+    position: myLatLng3,
+    map: map,
+    icon: 'amazonia.PNG'
   });
   }
