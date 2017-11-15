03f1a356cf245efca880ef70260906c7428a61e5
dmarti03x1 <31459768+dmarti03x1@users.noreply.github.com>
2017-11-14 18:06:55 -0800

    implementing 2nd marker
    this is my attempt at using 2 markers

--- javascript/jleaguemap.js
+++ javascript/jleaguemap.js
@@ -1,6 +1,7 @@
 function initMap() {
 
  var myLatLng = {lat: 41.867959, lng: -87.614014};
+ var myLatLng2 = {lat: 41.835337, lng: -87.628208};
 	
 	var map = new google.maps.Map(document.getElementById('map'), {
   zoom: 12,
@@ -11,5 +12,11 @@ function initMap() {
     position: myLatLng,
     map: map,
     icon: 'aqualad.PNG'
+  });
+	
+	var marker = new google.maps.Marker({
+    position: myLatLng2,
+    map: map,
+    icon: 'thebats.PNG'
   });
   }
