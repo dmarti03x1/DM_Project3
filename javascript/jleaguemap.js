9bb1975d95a353494d9bb792b999b77c83e6e760
dmarti03x1 <31459768+dmarti03x1@users.noreply.github.com>
2017-11-28 15:41:03 -0800

    adding line coordinates
    adding a line from one coordinate to another

--- javascript/jleaguemap.js
+++ javascript/jleaguemap.js
@@ -6,10 +6,12 @@ function initMap() {
  var myLatLng4 = {lat: 41.835063, lng: -87.663884};
  var myLatLng5 = {lat: 41.859065, lng: -87.660573};
  var myLatLng6 = {lat: 41.872777, lng: -87.675304};
+ 
 	
 	var map = new google.maps.Map(document.getElementById('map'), {
   zoom: 12,
   center: myLatLng
+	
   });
 	
 	var marker = new google.maps.Marker({
@@ -47,4 +49,18 @@ function initMap() {
     map: map,
     icon: 'soups.png'
   });
+	
+	var flightPlanCoordinates = [
+    {lat: 41.867959, lng: -87.614014}, 
+    {lat: 41.835337, lng: -87.628208}
+  ];
+	var flightPath = new google.maps.Polyline({
+    path: flightPlanCoordinates,
+    geodesic: true,
+    strokeColor: '#FF0000',
+    strokeOpacity: 1.0,
+    strokeWeight: 2
+  });
+	flightPath.setMap(map);
+	
   }
